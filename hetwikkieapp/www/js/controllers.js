f175c9cc087c82f55387bcda3421c3e11e5145f4
Menno Pietersen <info@anyscreensize.com>
2015-08-24 21:21:22 +0200

    Update

--- hetwikkieapp/www/js/controllers.js
+++ hetwikkieapp/www/js/controllers.js
@@ -41,41 +41,6 @@ angular.module('wikkie.controllers', [])
   };
 })
 
-.factory('dierService', function($http) {
-  var url="http://hetwikkie.nl/dieren.json";
-  var dierenData = function(){
-    return $http.get(url);
-  }
-  return {
-    getData: dierenData
-  }
-})
-
-.factory('dierDetailService', function($http) {
-  var dieren = [
-    {
-        "title": "Querney", 
-        "id": "0",
-        "diersoort": "Schaap",
-        "description": "Dit is Querney, één van onze gecastreerde rammen. Je herkent hem aan zijn gevlekte kop!"
-    },
-    {
-        "title":"Suske", 
-        "id": "1",
-        "diersoort": "Schaap",
-        "description": "Dit is Suske, één van onze gecastreerde rammen. Je herkent hem aan zijn gevlekte kop!"
-    }
-  ];
-  return {
-    all: function() {
-      return dieren;
-    },
-    get: function(dierId) {
-      return dieren[dierId];
-    }
-  }
-})
-
 .controller('dierenCtrl', function($scope, dierService) {
   dierService.getData()
   .success(function(data){
--- hetwikkieapp/www/js/services.js
+++ hetwikkieapp/www/js/services.js
@@ -1,2 +1,36 @@
 angular.module('wikkie.services', [])
 
+.factory('dierService', function($http) {
+  var url="http://hetwikkie.nl/dieren.json";
+  var dierenData = function(){
+    return $http.get(url);
+  }
+  return {
+    getData: dierenData
+  }
+})
+
+.factory('dierDetailService', function($http) {
+  var dieren = [
+    {
+        "title": "Querney", 
+        "id": "0",
+        "diersoort": "Schaap",
+        "description": "Dit is Querney, één van onze gecastreerde rammen. Je herkent hem aan zijn gevlekte kop!"
+    },
+    {
+        "title":"Suske", 
+        "id": "1",
+        "diersoort": "Schaap",
+        "description": "Dit is Suske, één van onze gecastreerde rammen. Je herkent hem aan zijn gevlekte kop!"
+    }
+  ];
+  return {
+    all: function() {
+      return dieren;
+    },
+    get: function(dierId) {
+      return dieren[dierId];
+    }
+  }
+})
